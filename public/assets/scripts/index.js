1b9abb77054afaf2e94a74c9f5119c7b111131da
xeho91 <xeho91@pm.me>
2021-02-28 15:00:06 +0100

    :tada: build: Added website project's fundamentals

---
+++ public/assets/scripts/index.js
@@ -0,0 +1,5 @@
+import App from "$source/App.svelte";
+
+new App({
+	target: document.getElementById("svelte"),
+});
---
+++ source/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import pathFavicon from "@xeho91/avatar";
+	import pathFontMonospace from "@xeho91/fonts/build/monospace/FiraCode.woff2";
+	import pathFontSansSerif from "@xeho91/fonts/build/sans-serif/WorkSans.woff2";
+	import pathFontSerif from "@xeho91/fonts/build/serif/Fraunces.woff2";
+</script>
+
+<svelte:head>
+	<link rel="icon" type="image/svg+xml" href="{pathFavicon}">
+	<link rel="preload" href="{pathFontMonospace}" as="font" type="font/woff2" crossorigin="anonymous">
+	<link rel="preload" href="{pathFontSansSerif}" as="font" type="font/woff2" crossorigin="anonymous">
+	<link rel="preload" href="{pathFontSerif}" as="font" type="font/woff2" crossorigin="anonymous">
+</svelte:head>
