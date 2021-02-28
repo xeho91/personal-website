b1b4e7b4631bb5c5812ae6ae176c23e85fc3d977
xeho91 <xeho91@pm.me>
2021-02-28 18:52:27 +0100

    :sparkles: feat(icons): Added SVG icons component

---
+++ source/components/Icon.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let symbol;
+</script>
+
+<style>
+	.icon {
+		--icon-size: 1em;
+
+
+		width: var(--icon-size);
+		width: var(--icon-size);
+
+		fill: var(--color-primary);
+	}
+</style>
+
+
+<svg class="icon">
+	<use href="./assets/images/icons.svg#{symbol}" />
+</svg>
---
+++ source/components/SocialMedia.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Icon from "$components/Icon.svelte";
+</script>
+
+<style>
+	ul {
+		display: flex;
+
+		padding: 0;
+	}
+
+	li {
+		padding-left: 1em;
+
+		list-style: none;
+	}
+</style>
+
+<div id="social-media">
+	<ul>
+		<li><a href="https://github.com/xeho91"><Icon symbol="github" /></a></li>
+		<li><a href="https://twitter.com/xeho91"><Icon symbol="twitter" /></a></li>
+		<li><a href="https://linkedin.com/in/xeho91"><Icon symbol="linkedin" /></a></li>
+	</ul>
+</div>
