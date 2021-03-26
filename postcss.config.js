71adba7f2e5e34a56e016f4664f9048549b5ef2f
xeho91 <xeho91@pm.me>
2021-03-26 19:37:26 +0800

    chore: :heavy_minus_sign: Removed uneccessary plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,10 @@
 module.exports = {
 	plugins: [
-		// https://github.com/postcss/postcss-url
-		require("postcss-url")(),
-
 		// https://github.com/csstools/postcss-preset-env
 		require("postcss-preset-env")({
 			stage: 1,
 			features: {
 				// https://preset-env.cssdb.org/features
-				// "nesting-rules": true,
 			},
 		}),
 
