6fda94cac7e1190655f67e21bb6e1844c2f20c61
xeho91 <xeho91@pm.me>
2021-05-28 17:16:47 +0800

    build(Scripts): ⚗  Support for older browsers

---
+++ src/scripts/checkFlexGap.ts
@@ -0,0 +1,22 @@
+export default function checkFlexGap(): boolean {
+	// create flex container with row-gap set
+	const  flex = document.createElement("div");
+
+	flex.style.display = "flex";
+	flex.style.flexDirection = "column";
+	flex.style.rowGap = "1px";
+
+	// create two, elements inside it
+	flex.appendChild(document.createElement("div"));
+	flex.appendChild(document.createElement("div"));
+
+	// append to the DOM (needed to obtain scrollHeight)
+	document.body.appendChild(flex);
+
+	// flex container should be 1px high from the row-gap
+	const isSupported = flex.scrollHeight === 1;
+
+	(flex.parentNode as HTMLElement).removeChild(flex);
+
+	return isSupported;
+}
