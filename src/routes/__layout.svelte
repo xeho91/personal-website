482a59adc1514d1c7224dc01777854f1cbd84bd7
xeho91 <xeho91@pm.me>
2021-05-28 17:31:41 +0800

    build(Svelte): 🏗  Updated entry templates

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,57 @@
+<script lang="typescript">
+	import { onMount } from "svelte";
+	import checkFlexGap from "$scripts/checkFlexGap";
+
+	import SiteHeader from "$lib/Site/Header.svelte";
+	import SiteContent from "$lib/Site/Content.svelte";
+	import SiteFooter from "$lib/Site/Footer.svelte";
+
+	onMount(() => {
+		if (!checkFlexGap()) {
+			document.body.classList.add("no-flex-gap");
+		}
+	});
+</script>
+
+<SiteHeader />
+<SiteContent><slot /></SiteContent>
+<SiteFooter />
+
+<style lang="postcss">
+	@import "$styles/global.css";
+
+	/*
+		FIXME:
+		For some reason, while building process (`npm run build`),
+		custom-media queries defined in `global.css`
+		are ignored in compiling Svelte components.
+		They work only inside the global.css file (along with imports).
+		Meanwhile, in the dev process (`npm run dev`), it works. 🤯
+		Intended or bug?
+	*/
+	@custom-media --motion (prefers-reduced-motion: no-preference);
+	@custom-media --no-motion (prefers-reduced-motion: reduce);
+
+	/* FIXME: Remove it when Safari starts to support Flex gap */
+	@custom-selector :--no-flex-gap :global(.no-flex-gap);
+	@custom-selector :--owl :global(> * + *);
+
+	:global(#svelte) {
+		--header-height: 5em;
+
+		--content-padding: min(2.5vw, 1em);
+
+		position: relative;
+
+		display: grid;
+		grid-template-areas:
+			"header"
+			"content"
+			"footer";
+		grid-template-rows:
+			var(--header-height)
+			calc(100% - var(--header-height))
+			auto;
+		grid-template-columns: 100vw;
+	}
+</style>
