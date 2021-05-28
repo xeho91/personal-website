401cfd00865f6710e2879d2b25cf074ced31403f
xeho91 <xeho91@pm.me>
2021-05-28 17:42:27 +0800

    build(TypeScript): 🔧  Improved config & support for external modules

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,4 +1,6 @@
 declare module "@iconify/svelte";
+declare module "svelte-inline-svg";
+declare module "cl-editor/src/Editor.svelte";
 
 /// <reference types="@sveltejs/kit" />
 /// <reference types="svelte" />
