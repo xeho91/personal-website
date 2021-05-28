86f7eae427c5e7ea5e704d1d6250607984f1ab2b
xeho91 <xeho91@pm.me>
2021-05-28 18:50:27 +0800

    feat(Buttons): 🎉  Added initial buttons components

---
+++ src/lib/Button/Circle.svelte
@@ -0,0 +1,95 @@
+<script lang="typescript">
+	import Case from "case";
+	import Icon from "@iconify/svelte";
+
+	import type { IconifyIcon } from "$types/IconifyIcon";
+
+	type ButtonType = "link" | "button";
+	type LabelPosition = "right" | "left" | "hidden";
+
+	export let type: ButtonType = "button";
+	export let icon: IconifyIcon;
+	export let label: string;
+	export let title: string;
+	export let label_position: LabelPosition = "right";
+
+	let id = $$props.id || `button_${Case.kebab(label)}`;
+</script>
+
+{#if type === "button"}
+	<button {id} {title} on:click>
+		{#if icon}
+			<Icon class="icon" {icon} />
+		{/if}
+		<span class="label {label_position}">{label}</span>
+	</button>
+{:else if type === "link"}
+	<a href={$$props.href} {id} {title} class="button">
+		{#if icon}
+			<Icon class="icon" {icon} />
+		{/if}
+		<span class="label {label_position}">{label}</span>
+	</a>
+{/if}
+
+<style lang="postcss">
+	button,
+	.button {
+		--button-size: 2.5em;
+
+		position: relative;
+
+		@mixin circle var(--button-size);
+		@mixin color;
+
+		@mixin background;
+		@mixin border;
+		@mixin box-shadow;
+
+		& > :global(.icon) {
+			size: 100%;
+
+			pointer-events: none;
+		}
+
+		& > .label {
+			--label-margin: 1ch;
+			--label-padding: 0.5em;
+			--color-foreground: var(--color_action-bg);
+			--color-background: transparent;
+
+			@mixin center-y absolute;
+
+			margin: 0 var(--label-margin);
+			padding: calc(var(--label-padding) / 2) var(--label-padding);
+
+			@mixin color;
+			white-space: nowrap;
+
+			@mixin background;
+			border-radius: calc(var(--label-padding) * 2);
+
+			pointer-events: none;
+
+			@media (--motion) {
+				transition: all var(--transition);
+			}
+
+			&.left {
+				right: 100%;
+			}
+			&.right {
+				left: 100%;
+			}
+			&.hidden {
+				opacity: 0;
+			}
+		}
+		&:hover > .label {
+			--color-foreground: var(--color_hovered-bg);
+		}
+		&:focus > .label {
+			--color-foreground: var(--color_focused-bg);
+		}
+	}
+</style>
---
+++ src/lib/Button/Normal.svelte
@@ -0,0 +1,50 @@
+<script lang="typescript">
+	import Case from "case";
+
+	import Icon from "@iconify/svelte";
+	import type { IconifyIcon } from "$types/IconifyIcon";
+	import iconLink from "@iconify-icons/carbon/link.js";
+
+	type ButtonType = "button" | "link";
+
+	export let type: ButtonType = "button";
+	export let label: string;
+	export let title: string;
+	export let icon: IconifyIcon | undefined = undefined;
+
+	export let disabled = false;
+
+	let id = `button_${Case.kebab(label)}`;
+</script>
+
+{#if type === "button"}
+	<button {id} {disabled} {title}>
+		<Icon {icon} inline={true} />
+		<span class="button-label">{label}</span>
+	</button>
+{:else if type === "link"}
+	<a {id} href={$$props.href || "#"} class="button" {title}>
+		<Icon icon={iconLink} inline={true} />
+		<span class="label">{label}</span>
+	</a>
+{/if}
+
+<style lang="postcss">
+	button,
+	.button {
+		--button-padding: 0.25em;
+
+		margin: 1ch;
+		padding: var(--button-padding) calc(var(--button-padding) * 2);
+
+		white-space: nowrap;
+
+		text-decoration: none;
+
+		border-radius: 1em;
+
+		@media (--motion) {
+			transition: all var(--transition);
+		}
+	}
+</style>
