25ead287d6e0948e7614c4f18359c6e150dd34a2
xeho91 <xeho91@pm.me>
2021-05-28 17:56:55 +0800

    build(Iconify): 🏷️  Added better support for Iconify

--- src/lib/Flag.svelte
+++ src/lib/Flag.svelte
@@ -1,48 +1,43 @@
-<script>
+<script lang="typescript">
 	// https://github.com/iconify/iconify/tree/master/packages/svelte
-	import IconifyIcon from "@iconify/svelte";
-	import { onMount } from "svelte";
+	import Icon from "@iconify/svelte";
 
-	import { mapVisible, mapShowCountry } from "$data/stores.js";
+	import { map } from "$data/stores";
+	import type { IconifyIcon } from "$types/IconifyIcon";
 
-	export let data;
-	export let name;
+	export let icon: IconifyIcon;
+	export let name: string;
 
-	function showMap() {
-		mapVisible.set(true);
-		mapShowCountry.set(name);
+	/** Show map in the DOM */
+	function handleClick() {
+		$map.hidden = false;
+		$map.showCountry = name;
 	}
-
-	onMount(async() => {
-		if (data instanceof Promise) {
-			try {
-				data = (await data)?.default;
-			} catch (error) {
-				console.error(error);
-			}
-		}
-
-	});
 </script>
 
-<span class="country" on:click={showMap}>
-	<IconifyIcon
-		icon={data}
+<span class="country" on:click={handleClick} title="Show on map">
+	<Icon
+		{icon}
 		class="flag"
 		width="1em"
 		height="1em"
-		inline="{true}"
-	/>{name}</span>
+		inline={true}
+	/>{name}
+</span>
 
-
-<style>
-	span {
-		text-decoration: underline;
-		text-decoration-style: dotted;
-	}
-</style>
 <!--
 	TODO:
 	Create a hover element which will display a map, with selected country
  -->
+<style lang="postcss">
+	.country {
+		white-space: nowrap;
+		text-decoration: underline;
+		text-decoration-style: dotted;
+		text-underline-offset: 0.1em;
 
+		& > :global(.flag) {
+			margin-right: 0.25em;
+		}
+	}
+</style>
