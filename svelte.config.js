45cb03bb3b84d9a595e532d5870096e3b0cd6ba6
xeho91 <xeho91@pm.me>
2021-05-28 19:17:12 +0800

    ci(Vercel): 🔧  Support deployment by Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import path from "path";
-import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-vercel";
 import preprocess from "svelte-preprocess";
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.cjs";
