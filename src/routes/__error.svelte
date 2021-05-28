5306b1d8f254995cb085663aa734f7bcf49f8656
xeho91 <xeho91@pm.me>
2021-05-28 18:55:34 +0800

    refactor(fallback): 🚧  Move to TypeScript & PostCSS

---
+++ src/routes/__error.svelte
@@ -0,0 +1,32 @@
+<svelte:head>
+	<title>xeho91 | 404 - page not found</title>
+</svelte:head>
+
+<h1>404</h1>
+<p>The page you are trying to visit doesn't exist.</p>
+
+<style lang="postcss">
+	h1 {
+		margin: 0;
+
+		font-size: 35vh;
+
+		user-select: none;
+	}
+
+	h1,
+	p,
+	a {
+		text-align: center;
+	}
+
+	a {
+		display: block;
+
+		width: fit-content;
+		margin: auto;
+		padding: 1em 0.5em;
+
+		border-radius: 10em;
+	}
+</style>
