6793ea9ec2efd5ce0bf38ff4e0ca8e956c25ec50
xeho91 <xeho91@pm.me>
2021-06-21 14:21:18 +0800

    fix(nodemailer): 🐛  fix tls issues

--- src/routes/api/email.ts
+++ src/routes/api/email.ts
@@ -5,17 +5,15 @@ import type { RequestHandler } from "@sveltejs/kit";
 import type { MailOptions } from "nodemailer/lib/smtp-pool";
 
 
+const isProduction = process.env["NODE_ENV"] === "production";
 /** Object with credentials for communicating with the SMTP mail server */
 const transport = createTransport({
 	host: process.env["NODEMAILER_HOST"],
 	port: Number(process.env["NODEMAILER_PORT"]),
-	secure: true,
+	secure: isProduction,
 	auth: {
 		user: process.env["NODEMAILER_USER"],
 		pass: process.env["NODEMAILER_PASSWORD"],
-	},
-	tls: {
-		rejectUnauthorized: true,
 	},
 });
 
